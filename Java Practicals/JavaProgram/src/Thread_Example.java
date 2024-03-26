cbfb6824c7c730e5393356e2dbb7b907bc57e003
Arumugam2002 <arumugampandaram65@gmail.com>
2024-03-26 10:22:06 +0530

    Thread and File Handling Practical added

---
+++ Java Practicals/JavaProgram/src/Thread_Example.java
@@ -0,0 +1,35 @@
+
+class FThread implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+class NewThread extends Thread{
+	
+}
+
+public class Thread_Example{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+			 Thread t = new Thread();
+			 System.out.println(t);
+			 t.setName("MyThread");
+			 
+			 System.out.println(t);
+			 
+			 for(int i=1;i<=5;i++)
+			 {
+				 System.out.println(i + " : " + t);
+				 Thread.sleep(3000);
+			 }
+			 
+			 System.out.println("Thread Complete");
+	}
+	
+}
