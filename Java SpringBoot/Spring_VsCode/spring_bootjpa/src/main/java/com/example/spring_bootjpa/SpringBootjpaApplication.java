89d6bc715f9c77c49d88dac03a76b800331503e7
Arumugam2002 <arumugampandaram65@gmail.com>
2024-07-10 16:51:37 +0530

    Springboot with rest controller added

---
+++ Java SpringBoot/Spring_VsCode/spring_bootjpa/src/main/java/com/example/spring_bootjpa/SpringBootjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_bootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootjpaApplication.class, args);
+	}
+
+}
